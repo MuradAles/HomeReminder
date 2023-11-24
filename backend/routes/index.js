064e40b97f1d6ce908b04ff213b211664adb55e1
MuradAles <muradaleskerov99@gmail.com>
2023-11-24 12:51:03 -0500

    user authorization backend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,9 @@
-// const userRoutes = require('./users');
+const userRoutes = require('./users');
 // const listsRoutes = require('./lists');
 // const roomsRoutes = require('./rooms');
 
 const constructorMethod = (app) => {
-    // app.use('/users', userRoutes);
+    app.use('/users', userRoutes);
     // app.use('/exercises', listsRoutes);
     // app.use('/routines', roomsRoutes);
     app.use('*', (req, res) => {
