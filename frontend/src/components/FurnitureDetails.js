ce938a3079ba75c50b57c47f329f214e0a92dbe8
Joy Choi <joychoi1002@gmail.com>
2023-12-06 22:47:38 -0500

    last styling and functions

--- frontend/src/components/FurnitureDetails.js
+++ frontend/src/components/FurnitureDetails.js
@@ -1,16 +1,43 @@
 import React from "react";
+import IconTrash from "../resources/IconTrash";
 
-function FurnitureDetails({ furnitureName, furnitureItem }) {
+function FurnitureDetails({ furnitureName, furnitureItem, onDelete }) {
   return (
-    <div>
-      <li>
+    <div
+      style={{
+        border: "2px solid #ddd",
+        borderColor: "#4973C2",
+        borderRadius: "16px",
+        padding: "8px",
+        marginBottom: "10px",
+        width: "90%",
+        backgroundColor: "#FAFAF5",
+        textAlign: "left",
+        margin: "20px auto",
+        position: "relative",
+      }}
+    >
+      <h2 style={{ fontSize: "24px", fontWeight: "bold", marginLeft: "5px" }}>
         {furnitureName}
-        <ul>
-          {furnitureItem.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
-      </li>
+      </h2>
+
+      <ul>
+        {furnitureItem.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+      <div
+        style={{
+          position: "absolute",
+          right: "8px",
+          top: "20px",
+          transform: "translateY(-50%)",
+          cursor: "pointer",
+        }}
+        onClick={() => onDelete(furnitureName)}
+      >
+        <IconTrash width={24} height={24} />
+      </div>
     </div>
   );
 }
--- frontend/src/components/RoomDetails.js
+++ frontend/src/components/RoomDetails.js
@@ -9,10 +9,10 @@ function RoomDetails({ selectedRoom, updateSelectedRoom }) {
   const [createFurnitureError, setCreateFurnitureError] = useState("");
   const [createFurnitureLoading, setCreateFurnitureLoading] = useState(false);
   const [furnitureAdded, setFurnitureAdded] = useState(false);
+  const [furnitureList, setFurnitureList] = useState([]);
   const [roomData, setRoomData] = useState(null);
 
   useEffect(() => {
-    console.log("useEffect triggered");
     const fetchData = async () => {
       try {
         const response = await fetch(`http://localhost:4000/rooms/get`, {
@@ -24,13 +24,14 @@ function RoomDetails({ selectedRoom, updateSelectedRoom }) {
         });
         const data = await response.json();
         setRoomData(data);
+        setFurnitureList(data?.furnitureList || []);
       } catch (error) {
         console.error("Error fetching data", error);
       }
     };
 
     fetchData();
-  }, [selectedRoom, newFurnitureName]);
+  }, [selectedRoom, roomData]);
 
   const handleAddFurniture = async (e) => {
     e.preventDefault();
@@ -86,25 +87,52 @@ function RoomDetails({ selectedRoom, updateSelectedRoom }) {
       setCreateFurnitureError("Error creating furniture");
     }
   };
+
+  const handleDeleteFurniture = async (furnitureName) => {
+    const updatedFurnitureList = { ...selectedRoom.furnitureList };
+    delete updatedFurnitureList[furnitureName];
+
+    updateSelectedRoom((prevSelectedRoom) => ({
+      ...prevSelectedRoom,
+      furnitureList: updatedFurnitureList,
+    }));
+
+    const endpoint = `http://localhost:4000/rooms/deleteFurnitureFromRoom`;
+    const furnitureInfo = {
+      roomId: selectedRoom._id,
+      furnitureName: furnitureName,
+    };
+
+    try {
+      const response = await fetch(endpoint, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(furnitureInfo),
+      });
+
+      const data = await response.json();
+      console.log(data);
+
+      if (data.error) {
+        // Handle error if necessary
+        console.error("Error deleting furniture:", data.error);
+      }
+    } catch (error) {
+      console.error("Error deleting furniture:", error);
+    }
+  };
+
   return (
-    <div key={selectedRoom._id}>
-      <h3>Room: {selectedRoom.roomName}</h3>
-      <input
-        type="text"
-        placeholder="Furniture Name"
-        value={newFurnitureName}
-        onChange={(e) => setNewFurnitureName(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Furniture Items"
-        value={newFurnitureItem}
-        onChange={(e) => setNewFurnitureItem(e.target.value)}
-      />
-      <Button type="submit" onClick={handleAddFurniture}>
-        Add Furniture
-      </Button>
-      <h4>Furnitures:</h4>
+    <div
+      key={selectedRoom._id}
+      style={{ marginTop: "8px", alignItems: "center", textAlign: "center" }}
+    >
+      <h3 style={{ fontSize: "40px", textAlign: "center" }}>
+        {selectedRoom.roomName}
+      </h3>
+
       {roomData && roomData.furnitureList ? (
         <ul>
           {Object.entries(roomData.furnitureList).map(
@@ -113,6 +141,7 @@ function RoomDetails({ selectedRoom, updateSelectedRoom }) {
                 key={index}
                 furnitureName={furnitureName}
                 furnitureItem={furnitureItem}
+                onDelete={handleDeleteFurniture}
               />
             )
           )}
@@ -120,6 +149,77 @@ function RoomDetails({ selectedRoom, updateSelectedRoom }) {
       ) : (
         <div> No furniture available</div>
       )}
+
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleAddFurniture();
+        }}
+      >
+        <div
+          style={{
+            borderTop: "1px solid #ccc",
+            marginTop: "16px",
+            display: "flex",
+            flexDirection: "column",
+            textAlign: "left",
+          }}
+        >
+          <label style={{ marginLeft: "20px", fontSize: "18px" }}>
+            {" "}
+            Name of Furniture:
+            <input
+              type="text"
+              placeholder="enter furniture name here"
+              value={newFurnitureName}
+              onChange={(e) => setNewFurnitureName(e.target.value)}
+              style={{
+                borderRadius: "20px",
+                padding: "4px",
+                width: "60%",
+                border: "2px solid #4973C2",
+                marginBottom: "20px",
+                marginTop: "20px",
+                marginBottom: "20px",
+                marginLeft: "12px",
+              }}
+            />
+          </label>
+
+          <label style={{ marginLeft: "20px", fontSize: "18px" }}>
+            {" "}
+            List of things:
+            <input
+              type="text"
+              placeholder="enter list here, separated by comma (e.g. condiments, utensils, napkins)"
+              value={newFurnitureItem}
+              onChange={(e) => setNewFurnitureItem(e.target.value)}
+              style={{
+                borderRadius: "20px",
+                padding: "4px",
+                width: "70%",
+                border: "2px solid #4973C2",
+                marginBottom: "20px",
+                marginLeft: "12px",
+              }}
+            />
+          </label>
+        </div>
+        <Button
+          type="submit"
+          onClick={handleAddFurniture}
+          style={{
+            width: "15%",
+            borderRadius: "20px",
+            padding: "6px",
+            marginBottom: "16px",
+            backgroundColor: "#4973C2",
+            fontWeight: "bold",
+          }}
+        >
+          Add Furniture
+        </Button>
+      </form>
     </div>
   );
 }
